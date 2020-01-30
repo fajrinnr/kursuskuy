b640de0c132171285facaaf17b3997ab59edb2f1
fajrinnr <fajrin.noorrachman11@gmail.com>
2020-01-30 13:52:24 +0700

    Add Homepage

--- seeders/20200130061538-Course.js
+++ seeders/20200130061538-Course.js
@@ -1,13 +1,22 @@
 'use strict';
 
-const
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('People', [{
-      name: ,
-      price: DataTypes.INTEGER
-    }], {});
+    return queryInterface.bulkInsert('Courses', [
+      {
+        name: 'Introduction to Javascript',
+        price: 150000,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Introduction to HTML',
+        price: 100000,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+  ], {});
   },
 
   down: (queryInterface, Sequelize) => {
