c8ca15b5f67ede58efc555c4a868a4ce9e6f5e10
Onesinus SPT <onesinus231@gmail.com>
2020-01-30 21:33:24 +0700

    Oke

--- seeders/20200130061542-User.js
+++ seeders/20200130061542-User.js
@@ -22,7 +22,7 @@ module.exports = {
       {
         username: 'admin',
         email: 'admin@kursuskuy.id',
-        password: '$2a$10$jVUktFMhTkB0vnqReinOXejd1irf3ESCtjqs5I.mrYVa3sb1R2DZa',
+        password: '$2a$10$jVUktFMhTkB0vnqReinOXejd1irf3ESCtjqs5I.mrYVa3sb1R2DZa', // 123
         role: 'admin',
         createdAt: new Date(),
         updatedAt: new Date()
