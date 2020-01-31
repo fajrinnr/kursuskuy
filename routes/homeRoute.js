c820b55d5831b6ba5e589677e33d34d4612bc642
fajrinnr <fajrin.noorrachman11@gmail.com>
2020-01-31 08:45:03 +0700

    KELAR BOS

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -7,6 +7,14 @@ Router.get('/', (req, res) => {
     res.render('home')
 })
 
+Router.use('/login', function (req, res, next) {
+    if(req.session.user){
+        res.redirect('/')
+    } else {
+        next()
+    }
+})
+
 Router.get('/login', GeneralController.loginForm)
 Router.get('/logout', GeneralController.logout)
 Router.get('/register', GeneralController.registerForm)
--- seeders/20200130061542-User.js
+++ seeders/20200130061542-User.js
@@ -4,22 +4,6 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
-        username: 'kursuskuy',
-        email: 'kursuskuy@gmail.com',
-        password: 'password',
-        role: 'admin',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        username: 'ihsankeren',
-        email: 'ihsankeren@gmail.com',
-        password: 'password',
-        role: 'member',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
         username: 'admin',
         email: 'admin@kursuskuy.id',
         password: '$2a$10$jVUktFMhTkB0vnqReinOXejd1irf3ESCtjqs5I.mrYVa3sb1R2DZa', // 123
